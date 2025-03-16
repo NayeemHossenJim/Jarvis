8757a92f64ef8bf7a656fc84b1b9050b69a3b31b
NayeemHossenJim <nayeemhossenjim@gmail.com>
2025-03-16 21:52:05 +0600

    Update music and open dundon

---
+++ Musiclibrary.py
@@ -0,0 +1,6 @@
+Music = {
+    "adele skyfall": "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D",
+    "begging" : "https://youtu.be/Qdkt3I5-FG4?si=XbUF02aY9nISEFij",
+    "slaver" : "https://youtu.be/yOb9Xaug35M?si=u2EGLASLzPsdEhkv",
+    "run" : "https://youtu.be/Fc7XWW_Ehb8?si=oCsR7l3psTCOxc-E"
+}
--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import speech_recognition as sr
 import webbrowser
 import pyttsx3
+import Musiclibrary
 
 recogniser = sr.Recognizer()
 engine = pyttsx3.init()
+
+def processcommand(c):
+    if "open youtube" in c.lower():
+        webbrowser.open("https://www.youtube.com/")
+    elif "open google" in c.lower():
+        webbrowser.open("https://www.google.com/")
+    elif "open facebook" in c.lower():
+        webbrowser.open("https://www.facebook.com/")
+    elif "open unreal engine documentation" in c.lower():
+        webbrowser.open("https://dev.epicgames.com/documentation/en-us/unreal-engine")
+    elif c.lower().startswith("play"):
+        song = c.lower().split(" ")[1]
+        Link = Musiclibrary.Music[song]
+        webbrowser.open(Link)
 
 def speak(text):
     engine.say(text)
@@ -20,6 +35,11 @@ if __name__ == "__main__":
             word =  r.recognize_google(audio)
             print(word)
             if(word.lower() == "jarvis"):
-                speak("Ya")
+                speak("Yes Boss")
+                with sr.Microphone() as source:
+                    print("Jarvis Active...")
+                    audio = r.listen(source)
+                    Command =  r.recognize_google(audio)
+                    processcommand(Command)
         except Exception as e:
             print("Error; {0}".format(e))
